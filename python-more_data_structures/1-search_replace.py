c2b9ec13289e1ac1d0140f805ab2ccd31852e497
root <km@rtin.mobi>
2022-09-12 22:20:55 -0700

    task1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    return [
+            [numbers, replace]
+            [numbers is search]
+            for numbers in my_list
+        ]
