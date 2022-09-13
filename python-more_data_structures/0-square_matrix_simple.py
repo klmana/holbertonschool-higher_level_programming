74734bb0b5b0bf71f0e90ce4880a9c2fd471d717
root <km@rtin.mobi>
2022-09-12 21:56:23 -0700

    fix Multiplication change to  Exponentiation

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    mynew_matrix = [[numbers * 2 for numbers in row] for row in matrix]
+    mynew_matrix = [[numbers ** 2 for numbers in row] for row in matrix]
     return mynew_matrix
