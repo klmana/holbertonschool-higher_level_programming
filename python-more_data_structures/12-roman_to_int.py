4ee105a5b1bd45812245d34e338ca39c14e7fde5
root <km@rtin.mobi>
2022-09-14 21:50:01 -0700

    task12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = 0
+    for number in range(len(roman_string)):
+        value = nums[roman_string[number]]
+        if number + 1 < len(roman_string) and nums[roman_string[number + 1]] > value:
+            sum -= value
+        else:
+            sum += value
+    return sum
