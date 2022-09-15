c6f6cf6cc87229246de6037dddc2d1430f8d0814
root <km@rtin.mobi>
2022-09-14 17:23:23 -0700

    task13

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    return (sum([number_0 * number_1 for number_0, number_1 in my_list])
+            / sum(number_1 for number_0, number_1 in my_list))
