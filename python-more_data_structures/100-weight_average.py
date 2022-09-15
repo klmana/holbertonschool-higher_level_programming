f97956a2179a89272b8bdeb65cf4bcd79a68ee33
root <km@rtin.mobi>
2022-09-14 17:28:53 -0700

    fix line break before binary

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,5 +2,4 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    return (sum([number_0 * number_1 for number_0, number_1 in my_list])
-            / sum(number_1 for number_0, number_1 in my_list))
+    return (sum([number_0 * number_1 for number_0, number_1 in my_list]) / sum(number_1 for number_0, number_1 in my_list))
