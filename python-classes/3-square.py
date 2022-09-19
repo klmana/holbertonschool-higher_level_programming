3e7c4b2fa88a7970b7349f01a2a1ae47a36647f7
root <km@rtin.mobi>
2022-09-19 03:45:18 -0700

    fix if not format

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" class for square. """
+""" class for Square """
 
 
 class Square:
     """Square class represents with the
     Private instance attribute: size
-    Instantiation with optional size"""
-    def __init__(self, size=0):
-        """initialize the class"""
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+    Instantiation with optional size
+    """
 
-        def area(self):
-            """Returns the current square area"""
-        return (self.__size**2)
+    def __init__(self, size=0):
+        """Initialize the class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
