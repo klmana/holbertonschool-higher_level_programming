b8447f3fa9348bda447292810837e5665246edb3
root <km@rtin.mobi>
2022-09-19 02:11:31 -0700

    task2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" class for square. """
+
+
+class Square:
+    """Square class represents with the Private instance attribute: size
+    Instantiation with optional size"""
+    def __init__(self, size=0):
+        """Initialize the class"""
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
