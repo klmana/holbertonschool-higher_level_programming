736a2b8c3d83aa22fca138a7fea7b80f7cb7c340
root <km@rtin.mobi>
2022-09-19 13:34:08 -0700

    task4

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" class for Square """
+
+
+class Square:
+    """ Square class represents.
+    Private instance attribute: size:
+    . property def size(self):to retrieve it
+    . property setter def size(self, value)to set it
+    .Instantiation with optional size.
+    .Public instance method: def area(self).
+    .that returns the current square area
+    """
+
+    def __init__(self, size=0):
+        """Initialize the class square."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property getter of  size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter the size to a value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
