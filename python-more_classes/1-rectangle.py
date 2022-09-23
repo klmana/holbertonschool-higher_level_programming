9522a890205267ac559c5aceaa16c177229fb6bb
root <km@rtin.mobi>
2022-09-22 18:43:26 -0700

    task1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Class for Rectangle """
+
+
+class Rectangle:
+    """ class defined by private
+        instance attribute width and height
+    """
+
+    def __init__(self, width=0, height=0):
+        """ Initialization of the rectangle method
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Set the width of Rectangle object
+        Returns:
+            Value of width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter Method of width
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set the height of Rectangle object
+        Returns:
+            Value of height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method of height
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
