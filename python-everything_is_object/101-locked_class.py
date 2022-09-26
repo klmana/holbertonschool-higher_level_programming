6ba16dadc0823b2f93b577c2c9d73f7774ea3c2d
root <km@rtin.mobi>
2022-09-25 22:42:07 -0700

    fix error case

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -4,4 +4,8 @@ class LockedClass:
     Locked class that prevents user dynamically create the instance
     attribute is called 'first_name'
     """
+
+    def __init__(self, first_name=""):
+        self.first_name = first_name
+
     __slots__ = ["first_name"]
