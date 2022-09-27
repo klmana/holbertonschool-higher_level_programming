ae29b635aebab9c560161e293d373984c76a5c38
root <km@rtin.mobi>
2022-09-26 23:51:38 -0700

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Create a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+      Geometry class Public instance method
+      def area(self)
+    """
+
+    def area(self):
+        """Raise an exception with the message area()
+           is not implemented
+        """
+        raise Exception('area() is not implemented')
