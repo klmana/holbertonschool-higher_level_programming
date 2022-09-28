170d73f8c08b297f7aecaeb4e398ca82bc39f679
root <km@rtin.mobi>
2022-09-27 23:36:27 -0700

    fix error the case

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -10,15 +10,15 @@ class MyInt(int):
       has == and != operators inverted
     """
 
-    def __ee__(self, other):
+    def __eq__(self, other):
         """
-          ee:equal equal method
+          eq:equal equal method
           to see if it knows how to compare itself to an int.
         """
-        return super().__ee__(other)
+        return super().__ne__(other)
 
     def __ne__(self, other):
         """
           ne:not equal method
         """
-        return super().__ne__(other)
+        return super().__eq__(other)
