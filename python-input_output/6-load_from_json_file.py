8beb6bd67ddbb94eb3cd0f231466ccb47c53d9fa
root <km@rtin.mobi>
2022-09-30 02:45:08 -0700

    task6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+  Function that creates
+  an Object from a “JSON file”:
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+      Create the object method from
+      Filename: File name of the “JSON file”:
+      Return: The object
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
