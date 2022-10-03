a98b73899a6c7e95be01c62223217fa229086e4c
root <km@rtin.mobi>
2022-10-02 19:39:51 -0700

    add private attributes with getter/setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -26,3 +26,63 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+        @property
+     def width(self):
+         """
+           Width getter
+         """
+         return self.__width
+
+     @width.setter
+     def width(self, value):
+         """
+           Width setter
+         """
+         self.integer_validator('width', value)
+         self.__width = value
+
+     @property
+     def height(self):
+         """
+           Height getter
+         """
+         return self.__height
+
+     @height.setter
+     def height(self, value):
+         """
+           Height setter
+         """
+         self.integer_validator('height', value)
+         self.__height = value
+
+     @property
+     def x(self):
+         """
+           x : getter
+         """
+         return self.__x
+
+     @x.setter
+     def x(self, value):
+         """
+           x : setter
+         """
+         self.integer_validator2('x', value)
+         self.__x = value
+
+     @property
+     def y(self):
+         """
+           y : getter
+         """
+         return self.__y
+
+     @y.setter
+     def y(self, value):
+         """
+           y : setter
+         """
+         self.integer_validator2('y', value)
+         self.__y = value
