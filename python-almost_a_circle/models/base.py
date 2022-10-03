32e3d369ef51284ded9a0a15ae860ce44a0c9659
root <km@rtin.mobi>
2022-10-02 18:53:15 -0700

    task1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+"""
+  Module:
+  This class will be the “base” of all other classes in this project.
+  The goal of it is to manage id attribute in all your future classes
+  and to avoid duplicating the same code (by extension, same bugs)
+"""
+
+
+class Base:
+    """
+      Base Class
+      Attributes:
+      _nb_objects: number of objects created
+      id: id of object an integer and need to test the type of it
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+          Init base method
+          args:
+          id: id of the  object
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
