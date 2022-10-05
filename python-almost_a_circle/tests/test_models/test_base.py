6eccec223c2a63659f626e98cbb4058efe557a01
root <km@rtin.mobi>
2022-10-05 15:34:39 -0700

    update base scenarios

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -19,22 +19,29 @@ class TestBase(unittest.TestCase):
         b = Base(5)
         self.assertEqual(b.id, 5)
 
+    def test_autoId(self):
+        '''
+        Test of Base for automatically assigning and incrementing an id
+        '''
+        b = Base()
+        self.assertEqual(b.id, 1)
+
     def setUpClass(cls):
         """
          Test setup of class
         """
 
-        cls.bs1 = Base()
-        cls.bs2 = Base(100)
-        cls.bs3 = Base()
+        b1 = Base()
+        b2 = Base(100)
+        b3 = Base()
 
     def tearDownClass(cls):
         """
           Test clear objects after all test
         """
-        del cls.bs1
-        del cls.bs2
-        del cls.bs3
+        del b1
+        del b2
+        del b3
 
     def test_output(self):
         """
@@ -78,9 +85,9 @@ class TestBase(unittest.TestCase):
         """
           Test Base initiation
         """
-        self.assertEqual(self.bs1.id, 1)
-        self.assertEqual(self.bs2.id, 100)
-        self.assertEqual(self.bs3.id, 2)
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 100)
+        self.assertEqual(self.b3.id, 2)
 
     def test_obj_id_exist(self):
         """
@@ -88,58 +95,6 @@ class TestBase(unittest.TestCase):
         """
         self.assertIsNotNone(self.bs1.id)
         self.assertIsNotNone(Base._Base__nb_objects)
-
-    def test_clsVar_match_id(self):
-        """
-          Test match class var to obj id
-        """
-        self.assertEqual(Base._Base__nb_objects, self.sq1.id)
-
-    def test_obj_id(self):
-        """
-          Test if id is assigning correctly
-        """
-        self.assertEqual(self.bs1.id, 1)
-
-    def test_base_methods(self):
-        """
-          Test if method exists in base
-        """
-        self.assertTrue(hasattr(Base, "__init__"))
-        self.assertTrue(hasattr(Base, "integer_validator"))
-        self.assertTrue(hasattr(Base, "integer_validator2"))
-        self.assertTrue(hasattr(Base, "to_json_string"))
-        self.assertTrue(hasattr(Base, "from_json_string"))
-        self.assertTrue(hasattr(Base, "save_to_file"))
-        self.assertTrue(hasattr(Base, "create"))
-        self.assertTrue(hasattr(Base, "load_from_file"))
-
-    def test_to_json(self):
-        """
-          Test save list to json
-        """
-        list1 = [
-            {'id': 100},
-            {'height': 88},
-            {'width': 1, 'id': 2, 'height': 88},
-            {'id': 4, 'height': 144, 'weight': 700},
-            {'width': 22, 'height': 11}
-        ]
-        empty = []
-
-        base_to_json = Base.to_json_string(list1)
-
-        base_to_empty_json = Base.to_json_string(empty)
-
-        self.assertIsInstance(list1, list)
-        self.assertIsInstance(base_to_json, str)
-
-        self.assertIsInstance(empty, list)
-        self.assertIsInstance(base_to_empty_json, str)
-
-        base_from_json = Base.from_json_string(rect_to_json)
-
-        self.assertIsInstance(base_from_json, list)
 
 
 if __name__ == "__main__":
