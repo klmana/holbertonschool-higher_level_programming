522bd75a95f4133766043c38a8b23b72e39692d8
root <km@rtin.mobi>
2022-10-05 19:15:24 -0700

    add  Base.from_json_string(None)

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -54,6 +54,30 @@ class TestBase(unittest.TestCase):
             Base.to_json_string([{1, 2}], [{3, 4}])
         self.assertEqual(error, str(index.exception))
 
+    def test_from_json_string(self):
+        """
+          Test from_json_string
+          That json string return to list
+        """
+        string = '[{"id": 1, "x": 2, "y": 3, "width": 4, "height": 5}]'
+        json_str = Base.from_json_string(string)
+        self.assertTrue(isinstance(string, str))
+        self.assertTrue(isinstance(json_str, list))
+        self.assertCountEqual(
+            json_str, [{'id': 1, 'x': 2, 'y': 3, 'width': 4, 'height': 5}])
+        json_string_1 = Base.from_json_string(None)
+        self.assertEqual(json_string_1, [])
+        error = ("from_json_string() missing 1 required positional argument:
+                 " + "'json_string'")
+        with self.assertRaises(TypeError) as index:
+            Base.from_json_string()
+        self.assertEqual(error, str(index.exception))
+        error = 'from_json_string()
+        takes 1 positional argument but 2 were given'
+        with self.assertRaises(TypeError) as index:
+            Base.from_json_string('[1]', '[2]')
+        self.assertEqual(error, str(index.exception))
+
 
 if __name__ == "__main__":
     unittest.main()
