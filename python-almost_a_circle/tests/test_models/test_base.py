e84709a2ecd0e18f24a72f5608a3142b2d8ed828
root <km@rtin.mobi>
2022-10-05 18:29:00 -0700

    test_base add import modul

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-'''
+"""
   Unit test for Base class
-'''
+"""
 
+import json
 import unittest
+import os
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    '''
+    """
       Unit tests suite for Base class
-    '''
+    """
+    def setUp(self):
+        Base.__nb_objects = 0
 
     def test_constantId(self):
-        '''
+        """
           Test of Base for correctly initializing an id
-        '''
+        """
         b = Base(5)
         self.assertEqual(b.id, 5)
 
     def test_autoId(self):
-        '''
+        """
           Test of Base for automatically assigning and incrementing an id
-        '''
+        """
         b = Base()
         self.assertEqual(b.id, 1)
 
@@ -35,14 +41,14 @@ class TestBase(unittest.TestCase):
         self.assertTrue(isinstance(diction, dict))
         self.assertTrue(isinstance(json_dic, str))
         self.assertCountEqual(
-            json_dic, '{['id': 1, 'x': 2, 'y': 3, 'width': 4, 'height': 5]}')
+            json_dic, '[{"id": 1, "x": 2, "y": 3, "width": 4, "height": 5}]')
         json_d_1 = Base.to_json_string(None)
         self.assertEqual(json_d_1, '[]')
         error = ('to_json_string() missing 1 required positional argument: ' +
                  "'list_dictionaries'")
         with self.assertRaises(TypeError) as index:
             Base.to_json_string()
-        self.assertEqual(error, str(i.exception))
+        self.assertEqual(error, str(index.exception))
         error = 'to_json_string() takes 1 positional argument but 2 were given'
         with self.assertRaises(TypeError) as index:
             Base.to_json_string([{1, 2}], [{3, 4}])
