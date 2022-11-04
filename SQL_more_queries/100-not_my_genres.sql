369d96ceaf748e54d56f0092df4a99618bfa918b
root <km@rtin.mobi>
2022-11-03 19:33:55 -0700

    Task17 fix _ with .

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -8,5 +8,5 @@ WHERE tv_genres.name NOT IN
    ON tv_show_genres.genre_id = tv_genres.id
    RIGHT JOIN tv_shows
    ON tv_shows.id=tv_show_genres.show_id
-   AND tv_show_title = 'Dexter')
+   AND tv_show.title = 'Dexter')
 ORDER BY tv_genres.name ASC;
