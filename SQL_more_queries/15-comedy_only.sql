c4e8d87dde208e9aac5d6fba684f383fab3c5f16
root <km@rtin.mobi>
2022-11-03 19:04:32 -0700

    Task16

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -6,4 +6,4 @@ ON tv_show_genres.genre_id = tv_genres.id
 AND tv_genres.name ='Comedy'
 INNER JOIN tv_shows
 ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title ASC;
