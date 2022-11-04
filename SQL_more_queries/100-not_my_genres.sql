5451879845ed9a2e6af9578972b92bec21be14f9
root <km@rtin.mobi>
2022-11-03 19:41:46 -0700

    Task17 fix typo s and replace right join by INNER

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -6,7 +6,7 @@ WHERE tv_genres.name NOT IN
    FROM tv_genres
    LEFT JOIN tv_show_genres
    ON tv_show_genres.genre_id = tv_genres.id
-   RIGHT JOIN tv_shows
+   INNER JOIN tv_shows
    ON tv_shows.id=tv_show_genres.show_id
-   AND tv_show.title = 'Dexter')
+   AND tv_shows.title = 'Dexter')
 ORDER BY tv_genres.name ASC;
