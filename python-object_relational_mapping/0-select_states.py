9c48f113ac17caacf7cdb3a9f2d6a91fe029be46
root <km@rtin.mobi>
2022-11-07 12:14:40 -0800

    Task0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+  Lists all states from the database hbtn_0e_0_usa:
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    # make a connection and 3 arguments take in
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()# create a cursor
+    # execute query
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # close all cursor and database
+    cur.close()
+    db.close()
