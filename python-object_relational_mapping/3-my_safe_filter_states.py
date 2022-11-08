c870b2df35a2f8111b1b2ce2d8abad20cdb840f7
root <km@rtin.mobi>
2022-11-07 16:52:01 -0800

    Task3 format space

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,7 @@ if __name__ == "__main__":
     cur = db.cursor()   # create a cursor
 
     # execute query
-    cur.execute("SELECT * FROM states WHERE name = %s \
-    ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name =%s ORDER BY states.id ASC", (argv[4],))
 
     # fetch and print
     rows = cur.fetchall()
