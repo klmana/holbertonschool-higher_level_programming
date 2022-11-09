7d8155239fff44930942b7244714e15a7bf166d7
root <km@rtin.mobi>
2022-11-08 23:31:51 -0800

    Task11 fix modul

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Adds the State object Louisiana to the database hbtn_0e_6_usa'''
+#Adds the State object Louisiana to the database hbtn_0e_6_usa
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -7,8 +7,7 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ == '__main__':
-
-    ''' create engine, metadata for stored objects'''
+    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
