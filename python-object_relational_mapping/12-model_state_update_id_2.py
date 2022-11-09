ffa1b5da4d9a55d4b6243a0c24cbf934cb6d792c
root <km@rtin.mobi>
2022-11-09 11:55:49 -0800

    Task12 fix modul

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -20,8 +20,8 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
 session = Session()
-state_ob_change = session.query(State).filter(State.id == 2).first()
-state_ob_change.name = 'New Mexico'
+state = session.query(State).filter(State.id == 2).first()
+state..name = 'New Mexico'
 
 session.commit()
 session.close()
