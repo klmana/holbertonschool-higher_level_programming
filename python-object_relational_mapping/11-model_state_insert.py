21b4fe2d4e5f4b6158ec1a63aa9a99ff285b8266
root <km@rtin.mobi>
2022-11-08 23:13:43 -0800

    Task11 fix modul

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-  Prints the first State object from the database hbtn_0e_6_usa
-"""
+# Adds the State object “Louisiana” to the database hbtn_0e_6_usa
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -15,7 +13,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 
