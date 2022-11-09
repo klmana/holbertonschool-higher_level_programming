e3fe4698be00359a9a78788e228040534857e102
root <km@rtin.mobi>
2022-11-08 23:34:59 -0800

    Task11 fix modul

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Adds the State object Louisiana to the database hbtn_0e_6_usa
+#Adds the State object Louisiana to the db
 
 from model_state import Base, State
 from sqlalchemy import create_engine
