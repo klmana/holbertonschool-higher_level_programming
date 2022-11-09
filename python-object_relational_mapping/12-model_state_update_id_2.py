13b9f47d5882fa9e3ff6751be945490e9359c8ce
root <km@rtin.mobi>
2022-11-09 11:58:31 -0800

    Task12 fix modul

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-  Changes the name of a State object from the database hbtn_0e_6_usa
+ changes the name of a State object from the database
 """
 
 from model_state import Base, State
@@ -8,20 +8,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
-
-if __name__ == '__main__':
-
-    # create engine, metadata for stored objects
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-
-session = Session()
-state = session.query(State).filter(State.id == 2).first()
-state..name = 'New Mexico'
-
-session.commit()
-session.close()
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+                          sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
