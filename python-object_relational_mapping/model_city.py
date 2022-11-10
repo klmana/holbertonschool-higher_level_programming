4514ccc8465cf2326a9b8a1d17d5782234e0f44d
root <km@rtin.mobi>
2022-11-09 16:18:23 -0800

    Task14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+  Class definition 'City'
+"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """
+      The definition of the  City
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
