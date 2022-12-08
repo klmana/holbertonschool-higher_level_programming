dee16edd8bf677afa3ef12363a03ad05b03981e2
klmana <104742971+klmana@users.noreply.github.com>
2022-12-08 21:33:18 +1000

    Task3 remove length

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2); // get all arguments, excluding the node and script name
 
-if (args.length === 0) {
+if (!args[0]) {
   console.log('No argument');
 } else {
   console.log(args[0]);
