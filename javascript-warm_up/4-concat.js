64f14c645e1aa65c23cf1933dadc1f7b6f5b14f5
klmana <104742971+klmana@users.noreply.github.com>
2022-12-09 10:21:02 +1000

    fix error format

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-const args = process.argv.slice(2); // get all arguments, excluding the node and script name
-
-// If there are less than 2 arguments, just print an empty string
-if (args.length < 2) {
-  console.log("");
-} else {
-  // Print the first argument, followed by " is ", followed by the second argument
-  console.log(`${args[0]} is ${args[1]}`);
-}
+// script that prints two arguments passed to it
+const a = process.argv;
+console.log(a[2] + ' is ' + a[3]);
