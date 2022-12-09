38bbf3f115f174e0a5776914461492c8871b1dd4
klmana <104742971+klmana@users.noreply.github.com>
2022-12-10 08:30:10 +1000

    Task3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// define and export class Rectangle
+module.exports = class Rectangle { // class Rectangle  (width, height)
+  constructor (width, height) { // constructor  (width, height)
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+};
