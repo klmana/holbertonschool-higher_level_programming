d36c4e017ab0ed82b30e8a75b741887d696f3b84
klmana <104742971+klmana@users.noreply.github.com>
2022-12-11 08:47:31 +1000

    Task10 fix semistandard

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,6 +2,6 @@
 // function that converts a number from base 10 to another base passed as argument:
 exports.converter = function (base) { // function converter to convert number from base 10 to another base
   return function (num) { // return function to convert number from base 10 to another base
-    return num.toString(base); // return num converted to base 
+    return num.toString(base); // return num converted to base
   };
 };
