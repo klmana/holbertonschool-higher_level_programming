a418a601b54c21c966aa52e01d08f52ae8afa6bc
klmana <104742971+klmana@users.noreply.github.com>
2022-12-11 08:26:09 +1000

    Task7

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list:
+exports.nbOccurences = function (list, searchElement) { // function nbOccurences
+  let count = 0;  
+  for (let i = 0; i < list.length; i++) { // for loop to iterate through list
+    if (list[i] === searchElement) {  // if list[i] is equal to searchElement then
+      count++;
+    }
+  }
+  return count;
+}
