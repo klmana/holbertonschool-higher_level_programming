b9fe26247fc8033796555683fef81d41e817db0e
klmana <104742971+klmana@users.noreply.github.com>
2022-12-12 12:34:35 +1000

    Task1 fix del throw error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that writes a string to a file.
 
-let filename = process.argv[2];
-let text = process.argv[3];
+const filename = process.argv[2];
+const text = process.argv[3];
 const fs = require('fs');
 fs.writeFile(filename, text, 'utf8', function (err) {
-  if (err) throw err;{
+  if (err) {
     console.log(err);
-  } 
+  }
 });
