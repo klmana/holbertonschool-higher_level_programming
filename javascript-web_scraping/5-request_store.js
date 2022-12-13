34f0161ddb6d658efb2fd4320e13696bad04f720
klmana <104742971+klmana@users.noreply.github.com>
2022-12-13 14:15:56 +1000

    Task5 fix correct output small,big,empty text

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,14 +2,20 @@
 // script that gets the contents of a webpage and stores it in a file.
 
 const url = process.argv[2];
-const filename = process.argv[3];
+const file = process.argv[3];
 const fs = require('fs');
 const request = require('request');
 
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(file, body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   } else {
-    fs.writeFile(filename, body, 'utf8');
+    console.log('Erorr Code:' + response.statusCode);
   }
 });
