79a24be0bd31048bb7021be521844388ccda7c40
klmana <104742971+klmana@users.noreply.github.com>
2022-12-13 13:58:04 +1000

    Task5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+
+const url = process.argv[2];
+const filename = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filename, body, 'utf8');
+  }
+});
