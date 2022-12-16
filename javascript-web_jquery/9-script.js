19b6ca3d79b24047d418ed349159aadb34a31e6c
klmana <104742971+klmana@users.noreply.github.com>
2022-12-16 10:59:19 +1000

    Task9

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+// JavaScript script that fetches from https://stefanbohacek.com/hellosalut/?lang=fr
+// and displays the value of hello from that fetch in the HTML tag DIV#hello.
+
+    $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data, textStatus) {
+        $('DIV#hello').text(data.hello);
+    });
+    
